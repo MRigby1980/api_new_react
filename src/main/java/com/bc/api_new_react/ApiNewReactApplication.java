85bf0c0b96ac1bc402ddfe40bbcec8d99a5800e3
Matt Rigby <MattRigby@bc.com>
2019-03-12 12:54:27 -0600

    Initial Commit

---
+++ src/main/java/com/bc/api_new_react/ApiNewReactApplication.java
@@ -0,0 +1,13 @@
+package com.bc.api_new_react;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiNewReactApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiNewReactApplication.class, args);
+    }
+
+}
